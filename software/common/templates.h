f27bd9b27695c2f2c5386b1edb526c8343062f56
Daniel Palmer <daniel@0x0f.com>
2025-02-05 20:05:42 +0900

    software: Start using templates for functions etc to reduce my level of crazy

--- software/common/templates.h
+++ software/common/templates.h
@@ -1,5 +1,15 @@
+; kate: syntax Intel x86 (NASM)
+; kate: replace-tabs off
+
 %macro GLOBAL_FUNC 1
 global func_%1
 func_%1:
+%endmacro
 
+%macro STATIC_FUNC 1
+func_%1:
+%endmacro
+
+%macro EXPORT_FUNC 1
+extern func_%1
 %endmacro
