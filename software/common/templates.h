94a35e9ae74d6430b5177f4076d2b3e7f17077a8
Daniel Palmer <daniel@0x0f.com>
2025-02-11 12:28:38 +0900

    software: allthethings: Refactoring

--- software/common/templates.h
+++ software/common/templates.h
@@ -1,9 +1,22 @@
 ; kate: syntax Intel x86 (NASM)
 ; kate: replace-tabs off
 
-%macro GLOBAL_FUNC 1
+
+%macro GLOBAL_FUNC_LOCALS 2
 global func_%1
 func_%1:
+%if %2 > 0
+	push bp
+	mov bp, sp
+	sub sp, (2 * %2)
+%endif
+%endmacro
+
+%macro GLOBAL_FUNC 1
+GLOBAL_FUNC_LOCALS %1,0
+%endmacro
+
+%macro GLOBAL_FUNC_END 0
 %endmacro
 
 %macro STATIC_FUNC 1
@@ -12,4 +25,12 @@ func_%1:
 
 %macro EXPORT_FUNC 1
 extern func_%1
+%endmacro
+
+%macro LOCAL_STORE 2
+	mov [bp - (2 + (%1 * 2))], %2
+%endmacro
+
+%macro LOCAL_LOAD 2
+	mov %1, [bp - (2 + (%2 * 2))]
 %endmacro
--- software/os/inc/console.h
+++ software/os/inc/console.h
@@ -3,4 +3,6 @@
 extern func_console_init
 extern func_console_availablechars
 extern func_console_getch
+extern func_console_process
+extern func_console_refresh
 extern func_console_enableints
